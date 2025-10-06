93fca51f8f1724b52d88123b9fb1342fc234981c
baylerblanchard <baylerblanchard@gmail.com>
2025-10-06 12:36:32 -0500

    trying to test reading the pdf file. next i need beau to send me examples for the insurance readings

--- pdf_read_test.rb
+++ pdf_read_test.rb
@@ -1,7 +1,17 @@
 require 'pdf-reader'
 
-pdf_file = PDF::Reader.new('/pdf_test.pdf')
+pdf_file = PDF::Reader.new('pdf_test.pdf')
 
 # insurance_data = {}
 
-puts "this is a test. The pdf file has #{pdf_file.page_cout} pages"
+puts "this is a test. The pdf file has #{pdf_file.page_count} pages"
+
+string_to_find = 'rea'
+
+pdf_file.pages.each do |page|
+  page_text = page.text
+
+  match_data = page_text.match(string_to_find)
+
+  puts match_data
+end
