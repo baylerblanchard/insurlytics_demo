4484745fb71cc60db32769b8d9b34537c5cd27c7
baylerblanchard <baylerblanchard@gmail.com>
2025-10-07 10:33:02 -0500

    added beaus test html and redid the code for the test pdf file

--- pdf_read_test.rb
+++ pdf_read_test.rb
@@ -6,12 +6,19 @@ pdf_file = PDF::Reader.new('pdf_test.pdf')
 
 puts "this is a test. The pdf file has #{pdf_file.page_count} pages"
 
-string_to_find = 'rea'
+string_to_find = 'rea' # The base string we are looking for
+regex_to_find = /#{string_to_find}/i # A case-insensitive regular expression of that string
+
+total_matches = 0
 
 pdf_file.pages.each do |page|
   page_text = page.text
 
-  match_data = page_text.match(string_to_find)
+  # Use `scan` with the case-insensitive regex to find all occurrences.
+  matches_on_page = page_text.scan(regex_to_find)
 
-  puts match_data
+  puts "Found #{matches_on_page.count} matches on page #{page.number}" if matches_on_page.any?
+  total_matches += matches_on_page.count
 end
+
+puts "\nTotal occurrences of '#{string_to_find}' found: #{total_matches}"
