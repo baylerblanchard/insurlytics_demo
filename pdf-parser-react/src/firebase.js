9e91d2dd4ca47e20f66acb1d7b04694e2c617f54
baylerblanchard <baylerblanchard@gmail.com>
2025-11-09 12:47:47 -0600

    updating firebase.js to support google sign in

--- pdf-parser-react/src/firebase.js
+++ pdf-parser-react/src/firebase.js
@@ -1,6 +1,9 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
+import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyBXAoeB_4Ucdmj-Kti3Cp22zN8DlJab04c",
@@ -14,3 +17,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+export const googleProvider = new GoogleAuthProvider();
