73a4d9cd85f51e329413a3fce16f1ad52c0adac2
baylerblanchard <baylerblanchard@gmail.com>
2025-11-09 12:59:00 -0600

    finished adding the login page changes

--- pdf-parser-react/src/firebase.js
+++ pdf-parser-react/src/firebase.js
@@ -1,22 +1,31 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-
-import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
+// Consolidated import: both getAuth and GoogleAuthProvider in one line
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyBXAoeB_4Ucdmj-Kti3Cp22zN8DlJab04c",
+
   authDomain: "insurlytics-demo.firebaseapp.com",
+
   projectId: "insurlytics-demo",
+
   storageBucket: "insurlytics-demo.firebasestorage.app",
+
   messagingSenderId: "334255668039",
+
   appId: "1:334255668039:web:cd3889a2ac3934dc766a62",
+
   measurementId: "G-NDR65LGKZE"
+
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 const analytics = getAnalytics(app);
 
+// Export auth and googleProvider
+export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
